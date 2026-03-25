1b77ad8e7f1f1b0a0e7c2ec82d988c439af8fa89
Elias <ekasa123@gmail.com>
2026-03-25 21:11:23 +0300

    add project

---
+++ js/main.js
@@ -0,0 +1,26 @@
+for (let i = 0; i >= 10; i++) {
+  if (i === 10) {
+    console.log("it's 10");
+  } else {
+    console.log("none");
+  }
+}
+function hello(a, b) {
+  return a + b;
+}
+
+console.log(hello(5, 7));
+
+function see(b, c) {
+  return b - c;
+}
+
+console.log(see(4, 8));
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+console.log(arr);
+
+arr.length = 0;
+
+console.log(arr);
